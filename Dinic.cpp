99fc850f6a91a2dfbfb098f5d2a46bc842426c28
TAOGenna <35399045+TAOGenna@users.noreply.github.com>
2020-09-16 02:38:07 -0500

    Update Dinic.cpp

--- Dinic.cpp
+++ Dinic.cpp
@@ -1,65 +1,48 @@
-// SPOJ FASTFLOW - AC
-// http://www.spoj.com/problems/FASTFLOW/
-#include <bits/stdc++.h>
-#define pb push_back
-#define mp make_pair
-#define fst first
-#define snd second
-#define ALL(s) s.begin(),s.end()
-#define SZ(x) int((x).size())
-#define fore(i,a,b) for(int i=a,to=b;i<to;++i)
-using namespace std;
-typedef long long ll;
-
-#define INF (1LL<<62)
-// Min cut: nodes with dist>=0 vs nodes with dist<0
-// Matching MVC: left nodes with dist<0 + right nodes with dist>0
 struct Dinic{
 	int nodes,src,dst;
 	vector<int> dist,q,work;
-	struct edge {int to,rev;ll f,cap;};
+	struct edge {int to,rev;lli f,cap;};
 	vector<vector<edge>> g;
 	Dinic(int x):nodes(x),g(x),dist(x),q(x),work(x){}
-	void add_edge(int s, int t, ll cap){
-		g[s].pb((edge){t,SZ(g[t]),0,cap});
-		g[t].pb((edge){s,SZ(g[s])-1,0,0});
+	void add_edge(int s, int t, lli cap){
+		g[s].pb((edge){t,sz(g[t]),0,cap});
+		g[t].pb((edge){s,sz(g[s])-1,0,0});
 	}
 	bool dinic_bfs(){
-		fill(ALL(dist),-1);dist[src]=0;
+		fill(all(dist),-1);dist[src]=0;
 		int qt=0;q[qt++]=src;
 		for(int qh=0;qh<qt;qh++){
 			int u=q[qh];
-			fore(i,0,SZ(g[u])){
+			rep(i,0,sz(g[u])){
 				edge &e=g[u][i];int v=g[u][i].to;
 				if(dist[v]<0&&e.f<e.cap)dist[v]=dist[u]+1,q[qt++]=v;
 			}
 		}
 		return dist[dst]>=0;
 	}
-	ll dinic_dfs(int u, ll f){
+	lli dinic_dfs(int u, lli f){
 		if(u==dst)return f;
-		for(int &i=work[u];i<SZ(g[u]);i++){
+		for(int &i=work[u];i<sz(g[u]);i++){
 			edge &e=g[u][i];
 			if(e.cap<=e.f)continue;
 			int v=e.to;
 			if(dist[v]==dist[u]+1){
-				ll df=dinic_dfs(v,min(f,e.cap-e.f));
+				lli df=dinic_dfs(v,min(f,e.cap-e.f));
 				if(df>0){e.f+=df;g[v][e.rev].f-=df;return df;}
 			}
 		}
 		return 0;
 	}
-	ll max_flow(int _src, int _dst){
+	lli max_flow(int _src, int _dst){
 		src=_src;dst=_dst;
-		ll result=0;
+		lli result=0;
 		while(dinic_bfs()){
-			fill(ALL(work),0);
-			while(ll delta=dinic_dfs(src,INF))result+=delta;
+			fill(all(work),0);
+			while(lli delta=dinic_dfs(src,INF))result+=delta;
 		}
 		return result;
 	}
 };
-
 
 int main(){
 	int n,m;
