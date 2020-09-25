48b75ae8598ba22fe4c2e2476d62630b8155d2f1
TAOGenna <35399045+TAOGenna@users.noreply.github.com>
2020-09-25 17:01:31 -0500

    Update Modular Exponentiation.cpp

--- Modular Exponentiation.cpp
+++ Modular Exponentiation.cpp
@@ -5,3 +5,10 @@ lli modpow(lli x, lli n, lli m) {// (x^n)%m
 	if (n%2 == 1) u = (u*x)%m;
 	return u;
 }
+
+lli binpow(lli a, lli b) {
+    if (b == 0) return 1;
+    lli res = binpow(a, b / 2);
+    if (b % 2) return res * res * a;
+    else return res * res;
+}
